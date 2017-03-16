10dda0ff586bc6ee4f363c4d114869362df350c9
GridTech <cdroach@live.com>
2017-03-16 18:30:11 -0500

    Add project files.

---
+++ Grid Vertex/Grid_Vertex.py
@@ -0,0 +1,30 @@
+import os
+import pickle
+
+def init():
+    global dir
+    dir = os.getcwd()
+    global running
+    running = False
+
+def send(data, app):
+    os.chdir(os.path.join(os.path.expanduser('~'), "Documents" ))
+    pickle_out = open('gridVertexData.gv', 'w')
+    pickle.dump(data, pickle_out)
+    pickle_out.close()
+    pickle_out = open('gridVertexApp.gv', 'w')
+    pickle.dump(app, pickle_out)
+    pickle_out.close()
+    os.chdir(dir)
+
+def receive():
+    os.chdir(os.path.join(os.path.expanduser('~'), "Documents" ))
+    pickle_in = open('gridVertexData.gv', 'r')
+    return(pickle.load(pickle_in))
+    os.chdir(dir)
+
+def app():
+    os.chdir(os.path.join(os.path.expanduser('~'), "Documents" ))
+    pickle_in = open('gridVertexApp.gv', 'r')
+    return(pickle.load(pickle_in))
+    os.chdir(dir)
